99c17aa237a0835b6c65d49cc3449a6d6576fd7b
Nurtilek2005 <nurtileks2005@gmail.com>
2023-04-24 22:13:52 +0600

    updated homework_2

--- homework_2/task_0/MyProgram.java
+++ homework_2/task_0/MyProgram.java
@@ -9,8 +9,8 @@
 
 package homework_2.task_0;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -18,28 +18,32 @@ public class MyProgram {
     public String findJewelsInStones(String jewels, String stones) {
         StringBuilder result = new StringBuilder();
         Map<String, Integer> jewelsCount = new HashMap<>();
-        for (String stone: stones.split("")) {
-            for (String jewel: jewels.split("")) {
+        for (String stone : stones.split("")) {
+            for (String jewel : jewels.split("")) {
                 if (!(Objects.equals(stone, jewel))) continue;
                 int count = jewelsCount.getOrDefault(jewel, 0);
                 jewelsCount.put(stone, count + 1);
             }
         }
-        for (Map.Entry<String, Integer> item: jewelsCount.entrySet()) {
+        for (Map.Entry<String, Integer> item : jewelsCount.entrySet()) {
             result.append(item.getKey());
             result.append(item.getValue().toString());
         }
         return result.toString();
     }
 
-    public static void main(String[] args) {
+    public void run() {
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Введите драгоценные камни");
         String jewels = iScanner.nextLine();
         System.out.println("Введите кучу камней");
         String stones = iScanner.nextLine();
-        MyProgram program = new MyProgram();
-        String result = program.findJewelsInStones(jewels, stones);
+        String result = this.findJewelsInStones(jewels, stones);
         System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        MyProgram program = new MyProgram();
+        program.run();
     }
 }
--- homework_2/task_1/MyProgram.java
+++ homework_2/task_1/MyProgram.java
@@ -9,8 +9,54 @@
 
 package homework_2.task_1;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class MyProgram {
+    public int promptInt(String message) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            if (!(Objects.equals(message.strip(), ""))) {
+                System.out.println(message);
+            }
+            System.out.print(">>> ");
+            String line = iScanner.nextLine();
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильное число!");
+            }
+        }
+    }
+
+    public int[] promptNumList(String message, int size) {
+        int[] numList = new int[size];
+        if (!(Objects.equals(message.strip(), ""))) {
+            System.out.println(message);
+        }
+        int index = 1;
+        while (index <= size) {
+            int number = promptInt("(" + index + "/" + size + ")");
+            numList[index - 1] = number;
+            index++;
+        }
+        return numList;
+    }
+
+    public void run() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите строку\n>>> ");
+        String line = iScanner.nextLine();
+        StringBuilder customOrderString = new StringBuilder();
+        int[] indexList = this.promptNumList("Введите индексы", line.length());
+        for (int index : indexList) {
+            customOrderString.append(line.charAt(index - 1));
+        }
+        System.out.println(customOrderString);
+    }
+
     public static void main(String[] args) {
-        
+        MyProgram program = new MyProgram();
+        program.run();
     }
 }
