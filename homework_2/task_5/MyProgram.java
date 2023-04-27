6b9d0784a4708e32ef1250b8e8a1fad4366ee599
Nurtilek <nurtileks2005@gmail.com>
2023-04-28 01:36:34 +0600

    updated homework_2

--- homework_2/task_5/MyProgram.java
+++ homework_2/task_5/MyProgram.java
@@ -11,85 +11,16 @@ import homework_2.task_5.calculator.Calculator;
 import java.util.Scanner;
 
 public class MyProgram {
-    public enum Operation {
-        PLUS,
-        MINUS,
-        MULTIPLE,
-        DIVIDE
-    }
-
-    public int getInt(String message) {
-        Scanner iScanner = new Scanner(System.in);
-        while (true) {
-            if (message != null) System.out.println(message);
-            String intoLine = iScanner.nextLine();
-            try {
-                return Integer.parseInt(intoLine);
-            } catch (NumberFormatException e) {
-                System.out.println("Incorrect number!");
-            }
-        }
-    }
-
-    public Object getOperation() {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.println("Select operation");
-        System.out.println("1 - PLUS");
-        System.out.println("2 - MINUS");
-        System.out.println("3 - MULTIPLE");
-        System.out.println("4 - DIVIDE");
-        System.out.print(">>> ");
-        String intoLine = iScanner.nextLine();
-        switch (intoLine) {
-            case "1":
-            case "plus":
-                return Operation.PLUS;
-            case "2":
-            case "minus":
-                return Operation.MINUS;
-            case "3":
-            case "multiple":
-                return Operation.MULTIPLE;
-            case "4":
-            case "divide":
-                return Operation.DIVIDE;
-            default:
-                System.out.println("Invalid operation");
-                return null;
-        }
-    }
-
-    public Object execOperation(int numberA, int numberB, Operation op) {
-        if (op.equals(Operation.PLUS)) {
-            return numberA + numberB;
-        } else if (op.equals(Operation.MINUS)) {
-            return numberA - numberB;
-        } else if (op.equals(Operation.MULTIPLE)) {
-            return numberA * numberB;
-        } else if (op.equals(Operation.DIVIDE)) {
-            return numberA / numberB;
-        } else {
-            return null;
-        }
-    }
-
     public void run() {
-        int number_1 = getInt("Enter number 1");
-        int number_2 = getInt("Enter number 2");
-        while (true) {
-            Object operation = getOperation();
-            if (!(operation instanceof Operation)) {
-                continue;
-            }
-            Object result = execOperation(number_1, number_2, (Operation) operation);
-            System.out.println(result);
-            break;
-        }
+        Calculator calculator = new Calculator();
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Введите математическое выражение");
+        double result = calculator.execute(iScanner.nextLine());
+        System.out.println("Результат данного выражения: " + result);
     }
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        Scanner iScanner = new Scanner(System.in);
-        System.out.println(calculator.execute(iScanner.nextLine()));
+        MyProgram program = new MyProgram();
+        program.run();
     }
 }
--- homework_2/task_5/calculator/Calculator.java
+++ homework_2/task_5/calculator/Calculator.java
@@ -2,17 +2,20 @@ package homework_2.task_5.calculator;
 
 import homework_2.task_5.calculator.operation.*;
 
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class Calculator {
     HashMap<String, IOperation> operations = new HashMap<>();
+    Logger logger = Logger.getLogger(this.getClass().getName());
 
     public Calculator() {
+        logger.info("Loading operations...");
         operations.put("+", new PlusOperation());
         operations.put("-", new MinusOperation());
         operations.put("/", new DivideOperation());
         operations.put("*", new MultipleOperation());
+        logger.info("Operations loaded!");
     }
 
     public double execute(String numbers) {
