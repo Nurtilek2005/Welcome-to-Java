3f0f150038aea4f25589158db35c8520c7bd17b8
Nurtilek <nurtileks2005@gmail.com>
2023-04-28 15:18:17 +0600

    updated homework_3

--- homework_3/task_1/MyProgram.java
+++ homework_3/task_1/MyProgram.java
@@ -7,9 +7,46 @@
 
 package homework_3.task_1;
 
-public class MyProgram {
-    public void run() {
+import java.util.*;
 
+public class MyProgram {
+    public int promptInt(String message) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            if (!(Objects.equals(message.strip(), ""))) {
+                System.out.println(message);
+            }
+            System.out.print(">>> ");
+            String line = iScanner.nextLine();
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильное число!");
+            }
+        }
+    }
+
+    public List<Integer> promptNumList(String message, int size) {
+        List<Integer> numList = new ArrayList<>();
+        if (!(Objects.equals(message.strip(), ""))) {
+            System.out.println(message);
+        }
+        int index = 1;
+        while (index <= size) {
+            int number = promptInt("(" + index + "/" + size + ")");
+            numList.add(number);
+            index++;
+        }
+        return numList;
+    }
+
+    public void run() {
+        int size = promptInt("Введите размер списка: ");
+        List<Integer> numList = promptNumList("Введите числа: ", size);
+        for (int i = 0; i < numList.size(); i++) {
+            numList.removeIf(integer -> integer % 2 == 0);
+        }
+        System.out.println(numList);
     }
 
     public static void main(String[] args) {
--- homework_3/task_2/MyProgram.java
+++ homework_3/task_2/MyProgram.java
@@ -8,9 +8,56 @@
 
 package homework_3.task_2;
 
-public class MyProgram {
-    public void run() {
+import java.util.*;
 
+public class MyProgram {
+    public int promptInt(String message) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            if (!(Objects.equals(message.strip(), ""))) {
+                System.out.println(message);
+            }
+            System.out.print(">>> ");
+            String line = iScanner.nextLine();
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильное число!");
+            }
+        }
+    }
+
+    public ArrayList<Integer> promptNumArrayList(String message, int size) {
+        ArrayList<Integer> numList = new ArrayList<>();
+        if (!(Objects.equals(message.strip(), ""))) {
+            System.out.println(message);
+        }
+        int index = 1;
+        while (index <= size) {
+            int number = promptInt("(" + index + "/" + size + ")");
+            numList.add(number);
+            index++;
+        }
+        return numList;
+    }
+
+    public double getAverageInNumArrayList(ArrayList<Integer> numArrayList) {
+        double average = 0;
+        for (int number: numArrayList) {
+            average += number;
+        }
+        return average / numArrayList.size();
+    }
+
+    public void run() {
+        int size = promptInt("Введите размер списка: ");
+        ArrayList<Integer> numArrayList = promptNumArrayList("Введите числа: ", size);
+        int maxInNums = Collections.max(numArrayList);
+        int minInNums = Collections.min(numArrayList);
+        double avgInNums = getAverageInNumArrayList(numArrayList);
+        System.out.println("MIN ELEMENT IN LIST: " + minInNums);
+        System.out.println("MAX ELEMENT IN LIST: " + maxInNums);
+        System.out.println("AVG ELEMENT IN LIST: " + avgInNums);
     }
 
     public static void main(String[] args) {
