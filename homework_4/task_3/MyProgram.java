844aeea350f824683de7b09c4594d7dc20100aaa
Nurtilek <nurtileks2005@gmail.com>
2023-05-03 13:12:05 +0600

    updated homework_4

--- homework_4/task_3/MyProgram.java
+++ homework_4/task_3/MyProgram.java
@@ -7,33 +7,54 @@
 
 package homework_4.task_3;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class MyProgram {
-    public List<String> randomPlanets() {
-        Random rand = new Random();
-        String[] planetNames = new String[]{"Меркурий", "Венера", "Земля", "Марс", "Юпитер", "Сатурн", "Уран", "Нептун", "Плутон"};
-        List<String> planets = new ArrayList<>();
-        for (int i = 0; i < planetNames.length; i++) {
-            int randIndex = rand.nextInt(0, planetNames.length);
-            planets.add(planetNames[randIndex]);
+    public int promptInt(String message) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            if (!(Objects.equals(message.strip(), ""))) {
+                System.out.println(message);
+            }
+            System.out.print(">>> ");
+            String line = iScanner.nextLine();
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильное число!");
+            }
         }
-        return planets;
+    }
+
+    public LinkedList<Integer> promptList(String message, int size) {
+        LinkedList<Integer> numList = new LinkedList<>();
+        if (!(Objects.equals(message.strip(), ""))) {
+            System.out.println(message);
+        }
+        for (int i = 1; i <= size; i++) {
+            int number = promptInt("(" + i + "/" + size + ") >>> ");
+            numList.add(number);
+        }
+        return numList;
+    }
+
+    public int calcNumberListSum(LinkedList<Integer> numList) {
+        int result = 0;
+        Iterator<Integer> iterator = numList.iterator();
+        while (iterator.hasNext()) {
+            result += iterator.next();
+        }
+        return result;
     }
 
     public void run() {
-        List<String> planets = randomPlanets();
-        Map<String, Integer> planetsCount = new HashMap<>();
-        for (String planet: planets) {
-            if (planetsCount.containsKey(planet)) {
-                int oldCount = planetsCount.get(planet);
-                planetsCount.replace(planet, oldCount + 1);
-                continue;
-            }
-            planetsCount.put(planet, 1);
-        }
-        System.out.println(planets);
-        System.out.println(planetsCount);
+        int size = promptInt("Введите размер списка: ");
+        LinkedList<Integer> numList = promptList("Введите элементы LinkedList: ", size);
+        System.out.println(calcNumberListSum(numList));
+        System.out.println(numList);
     }
 
     public static void main(String[] args) {
