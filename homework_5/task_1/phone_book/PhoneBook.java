c784f7994ddf8f37c39ba45dbbc10e6f3c02718a
Nurtilek <nurtileks2005@gmail.com>
2023-05-13 21:24:22 +0600

    update homework_5

--- homework_5/task_1/phone_book/PhoneBook.java
+++ homework_5/task_1/phone_book/PhoneBook.java
@@ -1,6 +1,8 @@
 package homework_5.task_1.phone_book;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,10 +12,22 @@ public class PhoneBook {
     private final File dataBase;
 
     public PhoneBook() {
+        FileWriter writer;
+        FileReader reader;
         this.dataPath = Path.of(".");
-        this.dataBase = new File("data.json");
+        this.dataBase = new File("data.csv");
         try {
             this.dataBase.createNewFile();
+            reader = new FileReader(this.dataBase);
+            writer = new FileWriter(this.dataBase);
+            writer.write("Имя,Фамилия,Отчество,Номера телефона\n");
+            writer.flush();
+            writer.close();
+            char [] a = new char[36];
+            reader.read(a);
+            for(char c : a)
+                System.out.print(c);
+            reader.close();
         } catch (IOException exception) {
             exception.printStackTrace();
         }
@@ -21,6 +35,5 @@ public class PhoneBook {
             System.out.println("Вы специально создали папку вместо файла?");
             System.exit(1);
         }
-
     }
 }
