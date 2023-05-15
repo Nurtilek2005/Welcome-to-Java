960f57816d1a56e17c8b197e819de5125a0e4d56
Nurtilek <nurtileks2005@gmail.com>
2023-05-15 12:04:37 +0600

    update homework_5

--- homework_5/task_1/phone_book/PhoneBook.java
+++ homework_5/task_1/phone_book/PhoneBook.java
@@ -10,24 +10,19 @@ import java.nio.file.Path;
 public class PhoneBook {
     private final Path dataPath;
     private final File dataBase;
+    private final String fieldnames = "Имя,Фамилия,Отчество,Номера телефона";
 
     public PhoneBook() {
         FileWriter writer;
-        FileReader reader;
         this.dataPath = Path.of(".");
         this.dataBase = new File("data.csv");
         try {
             this.dataBase.createNewFile();
-            reader = new FileReader(this.dataBase);
+            if (!(this.isBaseEmpty())) return;
             writer = new FileWriter(this.dataBase);
-            writer.write("Имя,Фамилия,Отчество,Номера телефона\n");
+            writer.write(this.fieldnames + "\n");
             writer.flush();
             writer.close();
-            char [] a = new char[36];
-            reader.read(a);
-            for(char c : a)
-                System.out.print(c);
-            reader.close();
         } catch (IOException exception) {
             exception.printStackTrace();
         }
@@ -35,5 +30,28 @@ public class PhoneBook {
             System.out.println("Вы специально создали папку вместо файла?");
             System.exit(1);
         }
+    }
+
+    public String[] getAllData() {
+        FileReader reader;
+        try {
+            reader = new FileReader(this.dataBase);
+            StringBuilder fileData = new StringBuilder();
+            char next = (char) reader.read();
+            while (next != (char) -1) {
+                fileData.append(next);
+                next = (char) reader.read();
+            }
+            reader.close();
+            return fileData.toString().split("\n");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean isBaseEmpty(){
+        String[] data = this.getAllData();
+        return data.length <= 1;
     }
 }
